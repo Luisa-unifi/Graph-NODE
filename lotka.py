f59dda0a4c69b54d105c54a94256b479324ee211
Luisa-unifi <100774311+Luisa-unifi@users.noreply.github.com>
2024-09-26 13:23:30 +0200

    NODE for Lotka Volterra system

---
+++ lotka.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  7 15:09:24 2023
+
+@author: Luisa
+
+based on: https://github.com/rtqichen/torchdiffeq
+"""
+
+import os
+import time
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchdiffeq import odeint_adjoint as odeint
+
+
+
+data_size=10000
+batch_time=10
+batch_size=20
+niters=2500
+test_freq=20
+viz=True
+
+
+device = torch.device('cuda:' + str(0) if torch.cuda.is_available() else 'cpu')
+
+true_y0 = torch.tensor([[0.2, 0.2]]).to(device)
+
+
+
+t = torch.linspace(0., 8., data_size).to(device)
+
+
+class Chen(nn.Module):
+
+    def forward(self, t,y):
+        return torch.tensor([[y.data[0][0]-y.data[0][0]*y.data[0][1], y.data[0][0]*y.data[0][1]-y.data[0][1]]]).to(device)
+
+   
+with torch.no_grad():
+    true_y = odeint(Chen(), true_y0, t, method='dopri5')
+
+
+
+def get_batch():
+    s = torch.from_numpy(np.random.choice(np.arange(data_size - batch_time, dtype=np.int64), batch_size, replace=False))
+    batch_y0 = true_y[s]  
+    batch_t = t[:batch_time]
+    batch_y = torch.stack([true_y[s + i] for i in range(batch_time)], dim=0)  # (T, M, D)
+    return batch_y0.to(device), batch_t.to(device), batch_y.to(device)
+
+
+
+def makedirs(dirname):
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+viz=True
+import matplotlib.pyplot as plt
+def visualize(true_y, pred_y, odefunc, itr):
+
+    if  viz:
+        makedirs('plots')
+        
+        fig = plt.figure(figsize=(12, 4), facecolor='white')
+        ax_traj = fig.add_subplot(121, frameon=False)
+        ax_phase = fig.add_subplot(122, frameon=False)
+
+        ax_traj.cla()
+        ax_traj.set_title('Trajectories')
+        ax_traj.set_xlabel('t')
+        ax_traj.set_ylabel('x,y')
+        ax_traj.plot(t.cpu().numpy(), true_y.cpu().numpy()[:, 0, 0], t.cpu().numpy(), true_y.cpu().numpy()[:, 0, 1], 'g-')
+        ax_traj.plot(t.cpu().numpy(), pred_y.cpu().numpy()[:, 0, 0], '--', t.cpu().numpy(), pred_y.cpu().numpy()[:, 0, 1], 'b--')
+        ax_traj.set_xlim(t.cpu().min(), t.cpu().max())
+        ax_traj.set_ylim(0, 5)
+
+        ax_phase.cla()
+        ax_phase.set_title('Phase Portrait')
+        ax_phase.set_xlabel('x')
+        ax_phase.set_ylabel('y')
+        ax_phase.plot(true_y.cpu().numpy()[:, 0, 0], true_y.cpu().numpy()[:, 0, 1], 'g-')
+        ax_phase.plot(pred_y.cpu().numpy()[:, 0, 0], pred_y.cpu().numpy()[:, 0, 1], 'b--')
+        ax_phase.set_xlim(0, 5)
+        ax_phase.set_ylim(0, 5)
+
+       
+
+        fig.tight_layout()
+        plt.savefig('plots/{:03d}'.format(itr))
+        plt.draw()
+        plt.pause(0.001)
+
+
+class ODEFunc(nn.Module):
+
+    def __init__(self):
+        super(ODEFunc, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(2, 50),
+            nn.Tanh(),
+            nn.Linear(50, 2),
+        )
+
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
+
+    def forward(self, t, y):
+        return self.net(y)
+
+
+
+if __name__ == '__main__':
+
+    ii = 0
+
+    func = ODEFunc().to(device)
+    
+    optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
+    end = time.time()
+
+
+    for itr in range(1, niters + 1):
+        optimizer.zero_grad()
+        batch_y0, batch_t, batch_y = get_batch()
+        pred_y = odeint(func, batch_y0, batch_t).to(device)
+       
+        loss = torch.mean(torch.abs(pred_y - batch_y))
+        loss.backward()
+        optimizer.step()
+
+
+        if itr % test_freq == 0:
+            with torch.no_grad():
+                pred_y = odeint(func, true_y0, t)
+                loss = torch.mean(torch.abs(pred_y - true_y))
+                print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                visualize(true_y, pred_y, func, ii)
+                ii += 1
+                
+        end = time.time()
